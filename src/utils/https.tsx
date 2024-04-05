f9c146a06b48b4ff5adc72dfea6d2d796467f14e
Lucas Inacio <lucas1inacio7souza@gmail.com>
2024-04-05 09:56:41 -0300

    chore: undefined

--- src/utils/https.tsx
+++ src/utils/https.tsx
@@ -1,5 +1,8 @@
 // a method that appends https if it's not there and its not http or localhost
-export function appendHttps(url: string) {
+export function appendHttps(url?: string) {
+    if (!url) {
+        return undefined
+    }
     if (!url.startsWith("http") && !url.includes("localhost")) {
         return `https://${url}`;
     }
